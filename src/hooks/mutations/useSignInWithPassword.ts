cd923ab6484b09d6fbe3a2abc5622c6f18dd527e
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-12-06 10:48:20 +0900

    chore(auth): 로그인 실패 시 토스트 에러 메시지 표시 추가

--- src/hooks/mutations/useSignInWithPassword.ts
+++ src/hooks/mutations/useSignInWithPassword.ts
@@ -1,8 +1,14 @@
 import { signInWithPassword } from "@/api/auth";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export function useSignInWithPassword() {
   return useMutation({
     mutationFn: signInWithPassword,
+    onError: (error) => {
+      toast.error(error.message, {
+        position: "top-center",
+      });
+    },
   });
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <App />
     </QueryClientProvider>
   </BrowserRouter>,
