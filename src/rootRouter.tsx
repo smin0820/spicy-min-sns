7b82faea67fb0ac1b1d6ca98afc863cd9f8c0ff2
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-12-08 06:28:42 +0900

    feat : 세션 기반 Auth 라우팅 가드 구현 (GuestOnly & MemberOnly 레이아웃 추가)

--- src/rootRouter.tsx
+++ src/rootRouter.tsx
@@ -7,19 +7,25 @@ import ResetPasswordPage from "./pages/resetPassword";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
 import GlobalLayout from "./components/layout/globalLayout";
+import GuestOnlyLayout from "./components/layout/guestOnlyLayout";
+import MemberOnlyLayout from "./components/layout/memberOnluLayout";
 
 export default function RootRouter() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        <Route element={<GuestOnlyLayout />}>
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        </Route>
 
-        <Route path="/" element={<HomePage />} />
-        <Route path="/post/:postId" element={<PostDetailPage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route element={<MemberOnlyLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post/:postId" element={<PostDetailPage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
