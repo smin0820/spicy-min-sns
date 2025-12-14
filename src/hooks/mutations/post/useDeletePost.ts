398acdd7fcda0896e96852b99ede7cf7f0306941
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-12-15 02:38:03 +0900

    feat : 포스트 추가, 수정, 삭제 시 캐시를 수정해서 화면에 즉각 반영 기능 추가

--- src/hooks/mutations/post/useDeletePost.ts
+++ src/hooks/mutations/post/useDeletePost.ts
@@ -1,9 +1,12 @@
 import { deleteImagesInPath } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { UseMuTationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: UseMuTationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletePost) => {
@@ -12,6 +15,10 @@ export function useDeletePost(callbacks?: UseMuTationCallback) {
       if (deletePost.image_urls && deletePost.image_urls.length > 0) {
         await deleteImagesInPath(`${deletePost.author_id}/${deletePost.id}`);
       }
+
+      queryClient.resetQueries({
+        queryKey: QUERY_KEYS.post.list,
+      });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
--- src/hooks/mutations/post/useUpdatePost.ts
+++ src/hooks/mutations/post/useUpdatePost.ts
@@ -1,12 +1,27 @@
 import { updatePost } from "@/api/post";
-import type { UseMuTationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { type Post, type UseMuTationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdatePost(callbacks?: UseMuTationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: updatePost,
-    onSuccess: () => {
+    onSuccess: (updatePost) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Post>(
+        QUERY_KEYS.post.byId(updatePost.id),
+        (prevPost) => {
+          if (!prevPost)
+            throw new Error(
+              `${updatePost.id}에 대해당하는 포스트를 캐시 데이터에서 찾을 수 없습니다.`,
+            );
+
+          return { ...prevPost, ...updatePost };
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
