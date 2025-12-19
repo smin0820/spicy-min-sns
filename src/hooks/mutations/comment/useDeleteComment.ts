c69917650e633a5ebb0d2845784d649cdffd3600
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-12-20 01:43:47 +0900

    feat : 댓글 추가, 수정, 삭제 시 화면 즉시 반영되게 수정

--- src/hooks/mutations/comment/useDeleteComment.ts
+++ src/hooks/mutations/comment/useDeleteComment.ts
@@ -1,12 +1,24 @@
 import { deleteComment } from "@/api/comment";
-import type { UseMuTationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import { type Comment, type UseMuTationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteComment(callbacks?: UseMuTationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
+    onSuccess: async (deletedComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deletedComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 캐시 데이터에 보관되어 있지 않습니다.");
+          return comments.filter((comment) => comment.id !== deletedComment.id);
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
